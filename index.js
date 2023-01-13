4f579ae315f0cb8e42cb9ea169d8ca10eb90f3fc
“riverhuang-batam” <riverhuang011@gmail.com>
2023-01-14 05:35:07 +0700

    using cors to give cross origin for http request & add nojsoncallback to remove jsonflickrfunction

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express();
-const axios = require('axios')
+const axios = require('axios');
+const cors = require('cors')
 
-app.get('/', (req, res) => {
-    axios.get('https://api.flickr.com/services/feeds/photos_public.gne?format=json') //getting data from flickr api with json format
+const corsOption = {
+    origin: 'http://localhost:3000', //can be only send response to localhost:3000
+    optionsSuccessStatus: 200
+}
+
+app.get('/', cors(corsOption), (req, res) => {
+    axios.get('https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1') //getting data from flickr api with json format
     .then(result => res.status(200).send(result.data)) // send the result if success
     .catch(err => console.log(err))
 })
