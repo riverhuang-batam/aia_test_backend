50ddf4d06a840bf7a6f02286bc282b863b516407
“riverhuang-batam” <riverhuang011@gmail.com>
2023-01-14 20:45:10 +0700

    add body parser to read req.body and create post method for searching image by tag

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const axios = require('axios');
-const cors = require('cors')
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const corsOption = {
-    origin: 'http://localhost:3000', //can be only send response to localhost:3000
-    optionsSuccessStatus: 200
-}
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', cors(corsOption), (req, res) => {
-    axios.get('https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1') //getting data from flickr api with json format
-    .then(result => res.status(200).send(result.data)) // send the result if success
-    .catch(err => console.log(err))
-})
+app.get("/", (req, res) => {
+  axios
+    .get(
+      "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1"
+    ) //getting data from flickr api with json format
+    .then((result) => res.status(200).send(result.data)) // send the result if success
+    .catch((err) => console.log(err));
+});
 
-app.listen(4000, () => console.log('it works'))
+app.post("/", (req, res) => {
+  const tag = req.body.tag; // request from front end
+  const tagUrl = tag && tag.replace(/ /g, "%20"); // change space in string to %20
+  axios
+    .post(
+      `https://api.flickr.com/services/feeds/photos_public.gne?tags=${tagUrl}&format=json&nojsoncallback=1`
+    ) //getting data by tags from flickr api with json format
+    .then((result) => res.status(200).send(result.data)) // send the result if success
+    .catch((err) => console.log(err));
+});
+app.listen(4000, () => console.log("it works"));
